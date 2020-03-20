36adf1f6d6f695de598d5c240a7bdd9cf4e51a2a
Michiel de Jong <michiel@unhosted.org>
2020-03-20 09:30:57 +0100

    Add webpack

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import { HelloWorld } from "./main.ts";
+
+// eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+window.addEventListener("DOMContentLoaded", () => {
+  document.write(HelloWorld());
+});
